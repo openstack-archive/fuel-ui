d3b914399eea9eb11e5a7107a3d7a41163ffb3bb
Alexandra Morozova <astepanchuk@mirantis.com>
2016-03-07 18:17:43 +0100

    Support for new Interface properties layout
     - added navigation tabs for interface properties
       navigation - offloading modes and MTU
    
    Change-Id: I72c456cba7ef3376b2e5ad6b3be721d3edf4d3ea

--- static/models.js
+++ static/models.js
@@ -947,10 +947,14 @@ models.Interface = BaseModel.extend({
       errors.push(i18n(ns + 'too_many_untagged_networks'));
     }
     var interfaceProperties = this.get('interface_properties');
-    if (interfaceProperties && interfaceProperties.mtu) {
-      var mtuValue = interfaceProperties.mtu;
-      if (mtuValue && (mtuValue < 42 || mtuValue > 65536)) {
-        errors.push(i18n(ns + 'invalid_mtu'));
+
+    if (interfaceProperties) {
+      var interfacePropertiesValidationErrors =
+        this.validateInterfaceProperties(interfaceProperties);
+      if (!_.isEmpty(interfacePropertiesValidationErrors)) {
+        errors.push({
+          interface_properties: interfacePropertiesValidationErrors
+        });
       }
     }
 
@@ -972,6 +976,17 @@ models.Interface = BaseModel.extend({
     ) errors.push(i18n(ns + 'vlan_range_intersection'));
 
     return errors;
+  },
+  validateInterfaceProperties(interfaceProperties) {
+    var errors = {};
+    var ns = 'cluster_page.nodes_tab.configure_interfaces.validation.';
+    var mtuValue = interfaceProperties.mtu;
+    if (mtuValue) {
+      if (mtuValue < 42 || mtuValue > 65536) {
+        errors.mtu = i18n(ns + 'invalid_mtu');
+      }
+    }
+    return _.isEmpty(errors) ? null : errors;
   }
 });
 
