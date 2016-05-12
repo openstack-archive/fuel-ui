0f9c4e1de610f680418636901a0a5306d8b26f36
Nikolay Bogdanov <nbogdanov@mirantis.com>
2016-05-12 11:27:40 +0300

    Apply advanced network configuration to deployed state fix
    After cluster deployment changes made on Network settings tab
    on Other section are not restored after pressing Load Deployed
    Settings button. Fixed now.
    
    Change-Id: Iaa598a81fc10a4b9dd1b3af0ac6d81af9de5e4b9
    Closes-bug: #1578653

--- static/models.js
+++ static/models.js
@@ -809,23 +809,20 @@ models.Settings = Backbone.DeepModel
     },
     updateSettings(settings, models, updateNetworkSettings) {
       _.each(this.attributes, (section, sectionName) => {
-        if (
-          updateNetworkSettings === true && section.metadata.group !== 'network' ||
-          updateNetworkSettings === false && section.metadata.group === 'network'
-        ) return;
+        var isNetworkGroup = section.metadata.group === 'network';
+        if ((updateNetworkSettings && isNetworkGroup) ||
+          (!updateNetworkSettings && !isNetworkGroup)) {
+          _.each(section, (setting, settingName) => {
+            // do not update hidden settings (hack for #1442143)
+            if (setting.type === 'hidden') return;
 
-        _.each(section, (setting, settingName) => {
-          if (
-            updateNetworkSettings === true && setting.group !== 'network' ||
-            updateNetworkSettings === false && setting.group === 'network'
-          ) return;
-
-          // do not update hidden settings (hack for #1442143)
-          if (setting.type === 'hidden') return;
-
-          var path = utils.makePath(sectionName, settingName);
-          this.set(path, settings.get(path), {silent: true, validate: false});
-        });
+            if (isNetworkGroup ||
+              (!updateNetworkSettings && setting.group !== 'network')) {
+              var path = utils.makePath(sectionName, settingName);
+              this.set(path, settings.get(path), {silent: true, validate: false});
+            }
+          });
+        }
       });
 
       this.mergePluginSettings();
