27ba1b52c470276b72a6e9f5d23fe416e17d8b7b
Nikolay Bogdanov <nbogdanov@mirantis.com>
2016-06-03 10:29:42 +0300

    Switching from Backbone' router to React'
    Implements blueprint react-router
    Change-Id: I73106d99c02d4baa19cb5c380bbfc52c2c29004c

--- static/views/cluster_page_tabs/nodes_tab_screens/add_nodes_screen.js
+++ static/views/cluster_page_tabs/nodes_tab_screens/add_nodes_screen.js
@@ -13,26 +13,19 @@
  * License for the specific language governing permissions and limitations
  * under the License.
 **/
-import $ from 'jquery';
 import _ from 'underscore';
 import React from 'react';
 import {NODE_LIST_SORTERS, NODE_LIST_FILTERS} from 'consts';
 import models from 'models';
+import utils from 'utils';
 import NodeListScreen from 'views/cluster_page_tabs/nodes_tab_screens/node_list_screen';
 
 var AddNodesScreen = React.createClass({
   statics: {
-    fetchData({cluster}) {
+    loadProps(params, cb) {
       var nodes = new models.Nodes();
-      nodes.fetch = function(options) {
-        return this.constructor.__super__.fetch.call(this, _.extend({data: {cluster_id: ''}},
-          options));
-      };
-      return $.when(
-        nodes.fetch(),
-        cluster.get('roles').fetch(),
-        cluster.get('settings').fetch({cache: true})
-      ).then(() => ({nodes}));
+      nodes.fetch = utils.fetchClusterProperties();
+      return nodes.fetch().then(() => cb(null, {nodes}));
     }
   },
   render() {
@@ -40,6 +33,7 @@ var AddNodesScreen = React.createClass({
       {... _.omit(this.props, 'screenOptions')}
       ref='screen'
       mode='add'
+      roles={this.props.cluster.get('roles')}
       nodeNetworkGroups={this.props.cluster.get('nodeNetworkGroups')}
       showRolePanel
       statusesToFilter={['discover', 'error', 'offline', 'removing']}
