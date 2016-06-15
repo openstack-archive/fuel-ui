f3aaf3088790c8ed5984bf9e5b0c09b26a70e131
Anton Zemlyanov <azemlyanov@mirantis.com>
2016-06-15 16:35:40 +0300

    Support pluggable NIC attributes
    Implements blueprint nics-and-nodes-attributes-via-plugin
    
    Change-Id: I5b5423be48cbe59ea60a6faa90a4bd4d5af7ece0

--- static/tests/functional/test_node_interfaces.js
+++ static/tests/functional/test_node_interfaces.js
@@ -59,19 +59,20 @@ registerSuite(() => {
       return this.remote
         .clickByCssSelector('.mtu .btn-link')
         .assertElementExists(
-          '.mtu-control',
+          '.mtu-section input[name="value"]',
           'MTU control is shown when navigating to MTU tab'
         )
-        .setInputValue('.mtu-control input', '2')
+        .setInputValue('.mtu-section input[name="value"]', '2')
         .assertElementExists(
-          '.has-error.mtu-control',
+          '.mtu-section .has-error',
           'Error styles are applied to MTU control on invalid value'
         )
-        .assertElementExists(
-          '.text-danger.mtu',
-          'Invalid style is applied to MTU in summary panel'
-        )
-        .setInputValue('.mtu-control input', '256')
+        // FIXME(jkirnosova) restore this check after adding use_custom_mtu setting
+        //.assertElementExists(
+        //  '.mtu .btn-link.text-danger',
+        //  'Invalid style is applied to MTU in summary panel'
+        //)
+        .setInputValue('.mtu-section input[name="value"]', '256')
         .assertElementExists(
           '.ifc-inner-container.has-changes',
           'Has-Changes style is applied'
@@ -79,7 +80,7 @@ registerSuite(() => {
         .clickByCssSelector('.mtu .btn-link')
         .sleep(500)
         .assertElementNotDisplayed(
-          '.mtu-control',
+          '.mtu-section input[name="value"]',
           'MTU control is hidden after clicking MTU link again'
         );
     },
