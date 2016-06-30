3684a19f5d5bd3d35a89efa47cdb5337658d9914
Ekaterina Khomyakova <ekhomyakova@mirantis.com>
2016-06-30 13:38:44 +0300

    Add tests for component registry suite for vCenter (dvs, contrail and nsxv plugins)
    With component registry mechanism plugins can set compatibility, incompatibility and requires between components.
    Current tests implement automatization to verify UI representation of components relations. They have to be run regularly on CI.
    
    To run tests you need to export variables PLUGIN_RPM (path to example-plugin rpm) and VENV (path to virtual env).
    
    Change-Id: I5d929da714a4c44515094df4e6edd3a7d93f4aa0
    Related-Bug: #1597710

---
+++ static/tests/functional/component_registry/component_helpers.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define([
+  'intern/dojo/node!lodash',
+  'intern/chai!assert',
+  'intern/dojo/node!leadfoot/Command'
+], function(_, assert, Command) {
+  _.defaults(Command.prototype, {
+    updatePlugin: function(files) {
+      return new this.constructor(this, function() {
+        return this.parent
+            .then(function() {
+              /*eslint-disable */
+              require(['intern/dojo/node!child_process'], function (child_process) {
+                child_process.exec('/bin/sh ${SCRIPT_PATH} ' + files,
+                  function (err, stdout, stderr) {
+                    if (stdout) console.log(stdout);
+                    if (stderr) console.log(stderr);
+                });
+              });
+              /*eslint-enable */
+            })
+            .sleep(250);  // wait for plugin update
+      });
+    },
+    newClusterFillName: function(modal) {
+      return new this.constructor(this, function() {
+        return this.parent
+          .clickByCssSelector('.create-cluster')
+          .then(function() {
+            return modal.waitToOpen();
+          })
+          .setInputValue('[name=name]', 'Temp');
+      });
+    },
+    deleteCluster: function(modal) {
+      return new this.constructor(this, function() {
+        return this.parent
+          .clickByCssSelector('button.delete-environment-btn')
+          .clickByCssSelector('button.remove-cluster-btn')
+          .then(function() {
+            return modal.waitToClose();
+          });
+      });
+    }
+  });
+});
