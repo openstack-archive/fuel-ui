ff04a8dbc1a95ff4df673f37326caa0a860ea9aa
Ekaterina Khomyakova <ekhomyakova@mirantis.com>
2016-06-30 13:38:44 +0300

    Add tests for component registry suite for vCenter (dvs, contrail and nsxv plugins)
    Change-Id: I5d929da714a4c44515094df4e6edd3a7d93f4aa0
    Related-Bug: #1597710

---
+++ static/tests/component_registry/test_addit_smile_sahara.js
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2016 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define([
+  'intern!object',
+  'intern/chai!assert',
+  'tests/functional/helpers',
+  'tests/functional/pages/common',
+  'tests/functional/pages/modal'
+], function(registerSuite, assert, helpers, Common, Modal) {
+
+  registerSuite(function() {
+    var common, modal;
+
+    return {
+      name: 'Wizard Page',
+      setup: function() {
+        common = new Common(this.remote);
+        modal = new Modal(this.remote);
+        return this.remote
+          .then(function() {
+            return common.getIn();
+          });
+      },
+      beforeEach: function() {
+        var clusterName = common.pickRandomName('Temp');
+        return this.remote
+          .clickByCssSelector('.create-cluster')
+          .then(function() {
+            return modal.waitToOpen();
+          })
+          .setInputValue('[name=name]', clusterName);
+      },
+      'Test compatible ceph, incompatible tun+vmware, requires sahara': function() {
+      // https://mirantis.testrail.com/index.php?/cases/view/842451
+
+        return this.remote
+          .pressKeys('\uE007')  // go to Compute
+
+          // Check that smile is disabled when vCenter is enabled
+          .clickByCssSelector('input[value=hypervisor\\:vmware]')
+          .pressKeys('\uE007')  // Networking
+          .pressKeys('\uE007')  // Storage
+          .pressKeys('\uE007')  // Additional Services
+          .clickByCssSelector('input[value=additional_service\\:sahara]')
+          .assertElementDisabled('input[value=additional_service\\:smile]',
+                                 'Smile checkbox is enabled with vCenter')
+
+          .clickByCssSelector('button.prev-pane-btn') // back to Storage
+          .clickByCssSelector('button.prev-pane-btn') // Networking
+          .clickByCssSelector('button.prev-pane-btn') // Compute
+          .clickByCssSelector('input[value=hypervisor\\:vmware]')
+          .pressKeys('\uE007')  // Networking
+
+          // Check that smile is disabled when Neutron with tunneling segmentation is enabled
+          .clickByCssSelector('input[value=network\\:neutron\\:ml2\\:vlan]')
+          .clickByCssSelector('input[value=network\\:neutron\\:ml2\\:tun]')
+          .pressKeys('\uE007')  // Storage
+          .pressKeys('\uE007')  // Additional Services
+          .clickByCssSelector('input[value=additional_service\\:sahara]')
+          .assertElementDisabled('input[value=additional_service\\:smile]',
+                                 'Smile checkbox is enabled with tunneling segmentation')
+
+          .clickByCssSelector('button.prev-pane-btn') // back to Storage
+          .clickByCssSelector('button.prev-pane-btn') // Networking
+
+          // Create cluster with VLAN + Ceph Block Storage + Sahara + smile
+          .clickByCssSelector('input[value=network\\:neutron\\:ml2\\:tun]')
+          .clickByCssSelector('input[value=network\\:neutron\\:ml2\\:vlan]')
+          .pressKeys('\uE007')  // Storage
+          .clickByCssSelector('input[value=storage\\:block\\:ceph]')
+          .pressKeys('\uE007')  // Additional Services
+          .clickByCssSelector('input[value=additional_service\\:sahara]')
+          .clickByCssSelector('input[value=additional_service\\:smile]')
+          .pressKeys('\uE007')  // Finish
+          .pressKeys('\uE007')  // Create
+          .then(function() {
+            return modal.waitToOpen().sleep(50);
+          })
+
+          // Delete created environment
+          .clickByCssSelector('button.delete-environment-btn')
+          .clickByCssSelector('button.remove-cluster-btn');
+      }
+    };
+  });
+});
---
+++ static/tests/functional/component_registry/test_addit_xen_sun.js
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016 Mirantis, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ **/
+
+define([
+  'intern!object',
+  'intern/chai!assert',
+  'tests/functional/helpers',
+  'tests/functional/pages/common',
+  'tests/functional/pages/modal'
+], function(registerSuite, assert, helpers, Common, Modal) {
+
+  registerSuite(function() {
+    var common, modal;
+
+    return {
+      name: 'Wizard Page',
+      setup: function() {
+        common = new Common(this.remote);
+        modal = new Modal(this.remote);
+        return this.remote
+          .then(function() {
+            return common.getIn();
+          });
+      },
+      beforeEach: function() {
+        var clusterName = common.pickRandomName('Temp');
+        return this.remote
+          .clickByCssSelector('.create-cluster')
+          .then(function() {
+            return modal.waitToOpen();
+          })
+          .setInputValue('[name=name]', clusterName);
+      },
+      'Test compatible ceilometer, incompatible hypervisor:libvirt:*, requires murano': function() {
+      // https://mirantis.testrail.com/index.php?/cases/view/842449
+
+        return this.remote
+          .pressKeys('\uE007')  // go to Compute
+
+          // Check that xen is not compatible with sun and rain hypervisors
+          .clickByCssSelector('input[value=hypervisor\\:test\\:sun]')
+          .assertElementDisabled('input[value=hypervisor\\:xen]',
+                                 'Xen checkbox is enabled with sun')
+          .clickByCssSelector('input[value=hypervisor\\:test\\:rain]')
+          .assertElementDisabled('input[value=hypervisor\\:xen]',
+                                 'Xen checkbox is enabled with sun + rain')
+          .clickByCssSelector('input[value=hypervisor\\:test\\:sun]')
+          .assertElementDisabled('input[value=hypervisor\\:xen]',
+                                 'Xen checkbox is enabled with rain')
+          .clickByCssSelector('input[value=hypervisor\\:test\\:rain]')
+
+          .clickByCssSelector('input[value=hypervisor\\:xen]')
+          .assertElementDisabled('input[value=hypervisor\\:test\\:sun]',
+                                 'Sun checkbox is enabled with xen')
+          .assertElementDisabled('input[value=hypervisor\\:test\\:rain]',
+                                 'Rain checkbox is enabled with xen')
+
+          // Try to create cluster with xen without Murano
+          .pressKeys('\uE007')  // Networking
+          .pressKeys('\uE007')  // Storage
+          .pressKeys('\uE007')  // Additional Services
+          .pressKeys('\uE007')  // Finish
+          .pressKeys('\uE007')  // Create
+          .then(function() {
+            return modal.waitToOpen().sleep(100);
+          })
+          .assertElementTextEquals('div.text-error', 'Requires [u\'additional_service:murano\'] ' +
+                                   'for \'hypervisor:xen\' components were not satisfied.',
+                                   'Error were not displayed')
+          .then(function() {
+            return modal.close();
+          });
+      },
+      'Test create cluster with xen+Murano+Ceilometer': function() {
+        return this.remote
+          // Create cluster with xen + Murano + Ceilometer
+          .pressKeys('\uE007')  // go to Compute
+          .clickByCssSelector('input[value=hypervisor\\:xen]')
+          .pressKeys('\uE007')  // Networking
+          .pressKeys('\uE007')  // Storage
+          .pressKeys('\uE007')  // Additional Services
+          .clickByCssSelector('input[value=additional_service\\:murano]')
+          .clickByCssSelector('input[value=additional_service\\:ceilometer]')
+          .pressKeys('\uE007')  // Finish
+          .pressKeys('\uE007')  // Create
+          .then(function() {
+            return modal.waitToOpen().sleep(50);
+          })
+
+          // Delete created environment
+          .clickByCssSelector('button.delete-environment-btn')
+          .clickByCssSelector('button.remove-cluster-btn');
+      }
+    };
+  });
+});
