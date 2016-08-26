92985abce4b46543e9634c86b4fb1d7536e16685
Georgy Kibardin <georgy@kibardin.name>
2016-08-26 08:00:33 +0300

    [WIP] Enabling randomly failing tests
    Change-Id: Ia96f9b92d1bb0164923ae84533eaf92247ee7e83

--- run_ui_func_tests.sh
+++ run_ui_func_tests.sh
@@ -117,6 +117,12 @@ function run_ui_func_tests {
         popd > /dev/null
     fi
 
+    cat $FUEL_WEB_ROOT/nailgun/test_run/test.yaml
+
+    sed -i 's/^APP_LOGLEVEL:.*$/APP_LOGLEVEL: "DEBUG"/' $FUEL_WEB_ROOT/nailgun/test_run/test.yaml
+  
+    cat $FUEL_WEB_ROOT/nailgun/test_run/test.yaml
+  
     SERVER_PORT=$NAILGUN_PORT \
     ARTIFACTS=$ARTIFACTS \
     ${GULP} functional-tests --no-transpile --suites=$testcase || result=1
