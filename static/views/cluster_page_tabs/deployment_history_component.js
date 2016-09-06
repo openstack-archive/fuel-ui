b97159d9a2915241392faae3c87fcb9d32b9a8f5
Julia Aranovich <jkirnosova@mirantis.com>
2016-09-06 10:41:18 +0300

    Fixes for deployment history
    * don't try to format not defined task time
    * don't display an empty task attribute in task details dialog
    * don't display label for transaction with empty graph type
    
    Related-Bug: #1593753
    
    Change-Id: I7c7530aa6cce43eef874719b0a1779609bd6497b

--- static/views/cluster_page_tabs/deployment_history_component.js
+++ static/views/cluster_page_tabs/deployment_history_component.js
@@ -574,7 +574,8 @@ var DeploymentHistoryTable = React.createClass({
               (task) => DEPLOYMENT_TASK_ATTRIBUTES
                 .map((attr) => {
                   if (attr === 'time_start' || attr === 'time_end') {
-                    return formatTimestamp(parseISO8601Date(task.get(attr)));
+                    return _.isNull(task.get(attr)) ?
+                      '' : formatTimestamp(parseISO8601Date(task.get(attr)));
                   } else if (attr === 'node_id') {
                     return renderNodeName.call(this, task.get(attr));
                   } else {
