1538e122627695d085f12822c87af823a99477c3
ekhomyakova <ekhomyakova@mirantis.com>
2016-12-13 16:47:05 +0400

    Add tests for NICs feature, adapt for plugins
    Change-Id: I299d857ce74e5f2373e1b8a8a2e2c69c7e350d28

--- static/tests/functional/component_registry/component_helpers.js
+++ static/tests/functional/component_registry/component_helpers.js
@@ -18,13 +18,15 @@ import _ from 'intern/dojo/node!lodash';
 import childProcess from 'intern/dojo/node!child_process';
 import Command from 'intern/dojo/node!leadfoot/Command';
 import 'tests/functional/helpers';
+import assert from 'intern/chai!assert';
 
 _.defaults(Command.prototype, {
-  updatePlugin(files) {
+  updatePlugin(params) {
     return new this.constructor(this, function() {
+
       return this.parent
         .then(() => {
-          childProcess.exec('/bin/sh ${SCRIPT_PATH} ' + files,
+          childProcess.exec('/bin/bash ${SCRIPT_PATH} ' + params,
             (err) => {
               if (err) return;
             });
@@ -40,20 +42,108 @@ _.defaults(Command.prototype, {
         .setInputValue('[name=name]', 'Temp');
     });
   },
+  newClusterWithPlugin(modal) {
+    return new this.constructor(this, function() {
+      return this.parent
+        .clickByCssSelector('.create-cluster')
+        .then(() => modal.waitToOpen())
+        .setInputValue('[name=name]', 'Temp')
+
+        .pressKeys('\uE007') // go to Compute
+        .pressKeys('\uE007') // Networking
+        .pressKeys('\uE007') // Storage
+        .pressKeys('\uE007') // Additional Services
+        .clickByCssSelector('input[name="additional_service:service_plugin_v5_component"]')
+
+        .pressKeys('\uE007') // Finish
+        .pressKeys('\uE007') // Create
+        .then(() => modal.waitToClose());
+    });
+  },
   assertNextButtonEnabled() {
     return new this.constructor(this, function() {
       return this.parent
-        .assertElementNotExists('button.next-pane-btn.disabled',
-                                'Next button is disabled');
+        .assertElementNotExists('button.next-pane-btn.disabled', 'Next button is disabled');
     });
   },
-  deleteCluster(modal) {
+  clickIfExists(cssSelector) {
     return new this.constructor(this, function() {
       return this.parent
-        .clickByCssSelector('button.delete-environment-btn')
+        .findAllByCssSelector(cssSelector).then(function (btn) {
+          if (btn.length == 1) {
+            btn[0].click();
+          }
+        }).end();
+    });
+  },
+  deleteCluster(modal, clusterPage) {
+    return new this.constructor(this, function() {
+
+      return this.parent
+        .clickIfExists('a.dashboard.cluster-tab')
+
+        .clickIfExists('.btn-danger')
+        .then(() => modal.waitToClose())
+
+        .clickIfExists('button.delete-environment-btn')
         .then(() => modal.waitToOpen())
-        .clickByCssSelector('button.remove-cluster-btn')
+
+        .clickIfExists('button.remove-cluster-btn')
         .then(() => modal.waitToClose());
     });
-  }
+  },
+  clickObjectByIndex(objectsCssSelector, index) {
+    return new this.constructor(this, function() {
+      return this.parent
+        .findAllByCssSelector(objectsCssSelector).then(function (nodes) {
+          if (index < 0) {
+            index = nodes.length + index;
+          }
+          nodes[index].click();
+        }).end();
+    });
+  },
+  selectPluginNICPropertyByIndex(index) {
+    return new this.constructor(this, function() {
+      return this.parent
+        .clickObjectByIndex('span.fuel_plugin_example_v5 button', index)
+    });
+  },
+  selectNodeByIndex(nodeIndex) {
+    return new this.constructor(this, function() {
+      return this.parent
+        .clickObjectByIndex('div.node-list.row div:nth-child(2) div.checkbox-group.pull-left',
+                            nodeIndex);
+    });
+  },
+  bondInterfaces(itfIndex1, itfIndex2) {
+    var itfList = '.ifc-list > div .ifc-header .common-ifc-name .custom-tumbler';
+    return new this.constructor(this, function() {
+      _this = this
+      return this.parent
+        .clickObjectByIndex(itfList, itfIndex1)
+        .clickObjectByIndex(itfList, itfIndex2)
+        .clickByCssSelector('.btn-bond');
+    });
+  },
+  assertAmountMatches(cssSelector1, cssSelector2, message) {
+    return new this.constructor(this, function () {
+      var amount;
+      return this.parent
+        .findAllByCssSelector(cssSelector1).then(function (elements) {
+          amount = elements.length;
+        }).end()
+        .findAllByCssSelector(cssSelector2).then(function (elements) {
+          return assert.equal(elements.length, amount, message);
+        }).end();
+    });
+  },
+  applyItfChanges() {
+    return new this.constructor(this, function () {
+      return this.parent
+        .assertElementEnabled('button.btn-apply', 'Apply is disabled')
+        .clickByCssSelector('button.btn-apply')
+        .waitForCssSelector('.btn-defaults:not(:disabled)', 1000);
+    });
+  },
 });
